7d79c1390959dd6e491610d313851776b5fc3796
jseo123 <jose123135@gmail.com>
2022-07-25 21:45:53 +0200

    Formatting

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,13 +4,13 @@ import { getConenction } from "../database";
 //example of a get request
 const dummyUsers  = async (req, res) => {
 try {
- const connection = await getConenction();
+    const connection = await getConenction();
  // all you need to do is to write the query on the next line
  // for the other controller functions just copy this one.
- const result     = await connection.query(
+    const result     = await connection.query(
     "SELECT * FROM  user"
- )
- res.json(result).status(200);
+    )
+    res.json(result).status(200);
 } catch(error) {
     res.status(500)
     res.send(error.message)
