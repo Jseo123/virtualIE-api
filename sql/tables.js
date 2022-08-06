e9b207131d671f3b521cb69bbbf6987d39b2645d
Omar Alsyouf <omaralsoyouf@gmail.com>
2022-08-06 16:49:06 +0100

    remove test table

--- sql/tables.js
+++ sql/tables.js
@@ -12,20 +12,6 @@ const tables = [
     UNIQUE INDEX email (email),
     UNIQUE INDEX username (username)
   )`,
-
-  `DROP TABLE IF EXISTS test`,
-
-  `CREATE TABLE test (
-    id INT NOT NULL AUTO_INCREMENT,
-    username VARCHAR(50) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    password VARCHAR(50) NOT NULL,
-    createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-    updatedAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
-    PRIMARY KEY (id),
-    UNIQUE INDEX email (email),
-    UNIQUE INDEX username (username)
-  )`,
 ];
 
 export default tables;
